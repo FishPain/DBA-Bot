491eaa0265244208c25d1c61f636ac204699b7f6
rishi8900 <rishi.kanneganti@gmail.com>
2021-04-23 16:21:53 +0800

    Missing comma
    lol

--- commands/index.js
+++ commands/index.js
@@ -2,6 +2,6 @@ module.exports = {
 	TT: require('./tt'),
 	IMH: require('./imh'),
 	Joke: require('./joke'),
-	Momma: reqiure('./momma')
+	Momma: reqiure('./momma'),
 	Sheesh: require('./sheesh'),
 };
